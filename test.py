a5c7a8bf4088b0e19061c29d100075b2f06b4572
unknown <miyukinakajima1994@gmail.com>
2019-05-23 23:39:25 +0800

    20190523233924

--- test.py
+++ test.py
@@ -4,12 +4,8 @@ http://www.yyearth.com/index.php?aid=241
 数据集的下载地址:
 http://www.yyearth.com/attachment.php?id=1234
 相关函数:
-cv2.xfeatures2d.SIFT_create
-cv2.FlannBasedMatcher
-cv2.BOWKMeansTrainer
-cv2.BOWImgDescriptorExtractor
-cv2.BOWImgDescriptorExtractor
-extract.compute
+
+
 bow_kmeans_trainer.add
 bow_kmeans_trainer.cluster
 extract_bow.setVocabulary
@@ -21,6 +17,7 @@ import cv2
 import numpy as np
 from os.path import join
 
+
 datapath = 'C:/Users/Administrator/PycharmProjects/openCV/dataset2/carData/TrainImages'
 
 def path(cls, i):
@@ -28,8 +25,8 @@ def path(cls, i):
 
 pos, neg = "pos-", "neg-"
 
-detect = cv2.xfeatures2d.SIFT_create()
-extract = cv2.xfeatures2d.SIFT_create()
+detect = cv2.HOGDescriptor()
+extract = cv2.HOGDescriptor()
 
 flann_params = dict(algorithm=1, trees=5)
 
@@ -46,43 +43,3 @@ def extract_sift(fn):
 for i in range(8):
     bow_kmeans_trainer.add(extract_sift(path(pos, i)))
     bow_kmeans_trainer.add(extract_sift(path(neg, i)))
-
-voc = bow_kmeans_trainer.cluster()
-extract_bow.setVocabulary(voc)
-
-def bow_features(fn):
-    im = cv2.imread(fn, 0)
-    return extract_bow.compute(im, detect.detect(im))
-
-traindata, trainlabels = [], []
-
-for i in range(20):
-    traindata.extend(bow_features(path(pos, i)))
-    trainlabels.append(1)
-    traindata.extend(bow_features(path(neg, i)))
-    trainlabels.append(-1)
-
-svm = cv2.ml.SVM_create()
-
-svm.train(np.array(traindata), cv2.ml.ROW_SAMPLE, np.array(trainlabels))
-
-def predict(fn):
-    f = bow_features(fn)
-    p = svm.predict(f)
-    print(fn, '\t', p[1][0][0])
-    return p
-
-car = r"C:\Users\Administrator\PycharmProjects\openCV\dataset2\carData\car.jpg"
-
-car_img = cv2.imread(car)
-
-car_predict = predict(car)
-
-font = cv2.FONT_HERSHEY_SIMPLEX
-
-if car_predict[1][0][0] == 1.0:
-    cv2.putText(car_img, 'Car Detected', (10, 30), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
-
-cv2.imshow('BOW + SVM Success', car_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
--- test2.py
+++ test2.py
@@ -1,30 +1,17 @@
 import cv2
-import numpy as np
 
 
-def is_inside(o, i):
-    ox, oy, ow, oh, = o
-    ix, iy, iw, ih = i
-    return ox > ix and oy > iy and ox + ow < ix + iw and oy + oh < iy + ih
+image_path = r'C:\Users\Administrator\PycharmProjects\openCV\dataset\example.png'
+image = cv2.imread(image_path)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+hod = cv2.HOGDescriptor()
+
+hod.detect(gray)
 
 
-image = cv2.imread('dataset/lena.png')
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-found, w = hog.detectMultiScale(image)
+result = hod.compute(image)
+print(result)
+print(result.shape)
+print(result.sum())
 
-found_filtered = []
-for ri, r in enumerate(found):
-    for qi, q in enumerate(found):
-        if ri != qi and is_inside(r, q):
-            break
-    else:
-        found_filtered.append(r)
-
-for person in found_filtered:
-    x, y, w, h = person
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 255), 2)
-
-cv2.imshow('people detection', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
