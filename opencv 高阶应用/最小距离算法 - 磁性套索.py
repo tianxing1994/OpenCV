9dd4a629ee335eae9906aefb55eb413021418ae2
unknown <miyukinakajima1994@gmail.com>
2019-06-01 13:25:25 +0800

    20190601132524

--- opencv 高阶应用/最小距离算法 - 磁性套索.py
+++ opencv 高阶应用/最小距离算法 - 磁性套索.py
@@ -1,6 +1,16 @@
+"""
+http://code.activestate.com/recipes/117228/
+https://github.com/pdyban/livewire
+磁性套索的基本原理应该是：
+高斯滤波或中值滤波 + 边缘提取 + 最短路径.
+对读入的图像, 首先进行中值滤波, 然后进行边缘提取, 输出为一个只有边缘的灰阶图像,
+然后再利用图论中的最短路径算法, 去寻找两个点之间的最短路径.
+那么这个最短路径就是磁性套索中检测到的两个点区域内的边缘 (最短路径).
+"""
 import heapq
 from math import log
-
+import cv2 as cv
+import numpy as np
 import math
 
 
@@ -14,13 +24,13 @@ def shortestPath(G, start, end, length_penalty=0.0):
     q = [(0, start, ())]
     visited = set()
     while True:
-        (cost, v1, path) = heapq.heappop(q)
+        (cost, v1, path) = heapq.heappop(q) # 从 heap 中取出最小的 item. 即 cost 损失最小的 item.
         if v1 not in visited:
             visited.add(v1)
             if v1 == end:
                 return list(flatten(path))[::-1] + [v1]
             path = (v1, path)
-            for (v2, cost2) in G[v1].iteritems():
+            for (v2, cost2) in G[v1].items():
                 if v2 not in visited:
                     heapq.heappush(q, (cost + cost2 + length_penalty * log(len(visited)), v2, path))
 
@@ -88,7 +98,7 @@ class LiveWireSegmentation(object):
 
                 dist = {}
                 for n in neighbors:
-                    dist[n] = norm_function(self.edges[row][col] - self.edges[n[0], n[1]])
+                    dist[n] = norm_function(self.edges[row, col] - self.edges[n[0], n[1]])
 
                 self.G[(row, col)] = dist
 
@@ -97,3 +107,51 @@ class LiveWireSegmentation(object):
             raise AttributeError("Load an image first!")
         path = shortestPath(self.G, from_, to_, length_penalty=length_penalty)
         return path
+
+
+def show_image(image):
+    cv.namedWindow('input image', cv.WINDOW_NORMAL)
+    cv.imshow('input image', image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return
+
+
+def demo1():
+    image_path = '../dataset/lena.png'
+    image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+    algorithm = LiveWireSegmentation(image, smooth_image=False, threshold_gradient_image=False)
+    result = algorithm.compute_shortest_path((200, 380), (150, 415), length_penalty=0.0)
+
+    contours = np.array(result)[np.newaxis, :, :]
+    # cv.drawContours(image, contours, 0, 255, 2)
+
+    algorithm.edges[contours[0, :, 0], contours[0, :, 1]] = 255
+    show_image(algorithm.edges)
+
+    # image[contours[0, :, 0], contours[0, :, 1]] = 255
+    # show_image(image)
+    return
+
+
+def demo2():
+    image_path = '../dataset/lena.png'
+    image_bgr = cv.imread(image_path)
+    image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+    algorithm = LiveWireSegmentation(image, smooth_image=True, threshold_gradient_image=True)
+    result = algorithm.compute_shortest_path((200, 380), (150, 415), length_penalty=0.0)
+
+    contours = np.array(result)[np.newaxis, :, :]
+    # cv.drawContours(image, contours, 0, 255, 2)
+
+    image_bgr[contours[0, :, 0], contours[0, :, 1], :] = (0, 0, 255)
+    show_image(image_bgr)
+
+    # image[contours[0, :, 0], contours[0, :, 1]] = 255
+    # show_image(image)
+    return
+
+
+if __name__ == '__main__':
+    demo2()
+
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-import heapq
 
 
-heap = [1, 2, 3]
+l = [1, 2, 3]
 
-item = (1, 2, 3)
+l2 = [3]
 
-heapq.heappush(heap, item)
-
-print(heap)
-print(type(heap))
+print(l + l2)
