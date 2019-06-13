d1a94a8e09454ca251cfb94b4170a62d532ed401
unknown <miyukinakajima1994@gmail.com>
2019-06-13 23:41:10 +0800

    20190613234109

--- FCN 代码学习/代码抄写项目一.py
+++ FCN 代码学习/代码抄写项目一.py
@@ -6,8 +6,33 @@ https://github.com/shekkizh/FCN.tensorflow
 好像有一个数据集, 但是下载不了, 网上也不知道去哪找.
 现在, 把链接里的所有代码抄写一遍, 并做笔记. 学习理解它, 先.
 """
+import numpy as np
+import scipy.misc as misc
 
 
+class BatchDatset(object):
+    files = []
+    images = []
+    annotations = []
+    image_options = dict()
+    batch_offset = 0
+    epochs_completed = 0
+
+    def __init__(self, records_list, image_options=None):
+        self.files = records_list
+        self.image_options = image_options
+        self._read_images()
+
+    def _read_images(self):
+        self._channels = True
+        self.images = np.array([self._transform(filename['image']) for filename in self.files])
+        self._channels = False
+        self.annotations = np.array([np.expand_dims(self._transform(filename['annotation']), axis=3) for filename in self.files])
+        return
+
+    def _transform(self, filename):
+        image = misc.imread(filename)
+        return
 
 
 
